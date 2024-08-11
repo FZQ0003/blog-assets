7bf521791cb27c8c0636e388c4b294771df8025a
FZQ0003 <468403876@qq.com>
2024-08-11 13:20:11 +0800

    Hide progress bar

--- download_utils.sh
+++ download_utils.sh
@@ -12,7 +12,7 @@ while read line; do
     info=($line)
     echo ${info[2]} ${info[0]} | sha256sum -c 2> /dev/null
     if [ $? != 0 ]; then
-        wget ${info[1]} -O ${info[0]}
+        wget ${info[1]} -O ${info[0]} -q
     fi
     chmod +x ${info[0]}
 done < "$_path/utils.txt"
