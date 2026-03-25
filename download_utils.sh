c8625545eba0de2c1ae7c9324ccef8dd67d4f6cf
FZQ0003 <468403876@qq.com>
2026-03-25 23:46:24 +0800

    Chore: wget -> curl

--- download_utils.sh
+++ download_utils.sh
@@ -13,7 +13,8 @@ while read line; do
     echo ${info[2]} ${info[0]} | sha256sum -c 2> /dev/null
     if [ $? != 0 ]; then
         echo "Downloading ${info[0]}..."
-        wget ${info[1]} -O ${info[0]} -q
+        # wget ${info[1]} -O ${info[0]} -q
+        curl -s -o ${info[0]} ${info[1]}
     fi
     chmod +x ${info[0]}
 done < "$_path/utils.txt"
